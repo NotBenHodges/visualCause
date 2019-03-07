58fb53ee950f5ec8426c7034f85dbd9960efe226
Ben Hodges <ben.hodges@centre.edu>
2019-03-07 10:56:47 -0500

    a lot of work

--- cause.js
+++ cause.js
@@ -1,24 +1,35 @@
-var causeDataP = d3.csv("data.csv")
-var drawChart = function(data)
-{
+var data = [66,61,55,67]
   var h = 500;
   var w = 500;
-  var barWidth = width/data.length;
+  var barWidth = w/data.length;
   var svg = d3.select("body")
-              .append("svg");
+              .append("svg")
               .attr("height",h)
               .attr("width",w);
+
   svg.selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x",function(d,i){
-        return h - d.num * 20;
+      .attr("x", function(d,i){
+        return i * (w/data.length);
       })
-      .attr("width",barWidth)
-      .attr("h",function(d){
-        return d.num * 20;
+      .attr("y", function(d){
+        return h - d;
       })
-      .attr("fill","blue")
-      
-}
+      .attr("width",50)
+      .attr("height",function(d){
+        return d;
+      })
+
+
+
+/*causeDataP.then(function(data)
+{
+  console.log("data",data)
+  drawChart(data);
+},
+function(err)
+{
+  console.log(err);
+})*/
