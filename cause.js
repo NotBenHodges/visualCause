0825160b90fb0819a47dc10827d999656312a3c5
Ben Hodges <ben.hodges@centre.edu>
2019-03-07 10:00:05 -0500

    added csv

--- cause.js
+++ cause.js
@@ -1,43 +1,24 @@
-var incomeP = d3.csv("data.csv");
+var causeDataP = d3.csv("data.csv")
 var drawChart = function(data)
 {
-  console.log(data);
-  var height = 400;
-  var width = 600;
+  var h = 500;
+  var w = 500;
   var barWidth = width/data.length;
   var svg = d3.select("body")
-              .append("svg")
-              .attr("height",height)
-              .attr("width",width);
-
+              .append("svg");
+              .attr("height",h)
+              .attr("width",w);
   svg.selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x",function(d,i)
-      {
-        return i * barWidth;
-      })
-      .attr("y",function(d) {
-        return height - d.num * 20;
+      .attr("x",function(d,i){
+        return h - d.num * 20;
       })
       .attr("width",barWidth)
-      .attr("height",function(d)
-      {
+      .attr("h",function(d){
         return d.num * 20;
       })
-      .attr("fill",function(d)
-    {
-      return d.color;
-    })
-
-incomeP.then(function(data)
-{
-  console.log("data", data);
-  drawChart(data);
-},
-  function(err)
-{
-  console.log(err);
-})
+      .attr("fill","blue")
+      
 }
